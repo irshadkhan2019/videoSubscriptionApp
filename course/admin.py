85c28ff08a52b104fbf723053728a91e8d056c32
Irshad Khan <khanirshad2019@gmail.com>
2023-10-11 10:11:25 +0530

    Creating Courses

--- course/admin.py
+++ course/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Course
 # Register your models here.
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display=("title","description","price","is_free")
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(Course, CourseAdmin)
--- course/models.py
+++ course/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    is_free = models.BooleanField(default=True) 
+    image = models.ImageField(upload_to='course_images/', blank=True, null=True)  
